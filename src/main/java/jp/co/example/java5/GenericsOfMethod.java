56a88a4b611b1a8ce8acd32cce0c8cd76b342708
fukugit <fukuma1234@gmail.com>
2019-01-18 08:42:46 +0900

    Add class for Generics

---
+++ src/main/java/jp/co/example/java5/GenericsOfMethod.java
@@ -0,0 +1,13 @@
+package jp.co.example.java5;
+
+public class GenericsOfMethod {
+
+  public static void main(String[] args) {
+    
+  }
+
+  private <T> T get(T name) {
+    return name;
+  }
+
+}
