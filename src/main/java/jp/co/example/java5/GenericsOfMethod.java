bbf68ff119024a2f8334eb7c1800f80874981b1f
fukugit <fukuma1234@gmail.com>
2019-01-30 23:27:41 +0900

    Add Generics class

--- src/main/java/jp/co/example/java5/GenericsOfMethod.java
+++ src/main/java/jp/co/example/java5/GenericsOfMethod.java
@@ -1,5 +1,6 @@
 package jp.co.example.java5;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,15 +9,18 @@ import java.util.Map;
 public class GenericsOfMethod {
 
   public static void main(String[] args) {
-    // Using generics method, whose result is single.
+    // The below is good site for Generics.
+    // https://qiita.com/rodentia/items/b36d134fa24867ba4d63
+    
+    ////// Using generics method, whose result is single.
     String test1 = get("Test1");
     System.out.println(test1);
     
-    // Using generics method, whose result is list.
+    ////// Using generics method, whose result is list.
     List<String> test2 = getList("Test2");
     test2.stream().forEach(System.out::println);
     
-    // Able to be Type inference(型推論)
+    ////// To define the Type inference(型推論) class.
     Map<String, Integer> map1 = getHashMap();
     map1.put("key", 1);
     map1.entrySet().stream().map(e -> e.getKey() + ": " + e.getValue()).forEach(System.out::println);
@@ -25,14 +29,16 @@ public class GenericsOfMethod {
     map2.put("key", "val");
     map2.entrySet().stream().map(e -> e.getKey() + ": " + e.getValue()).forEach(System.out::println);
     
-    // extends
+    ////// Using single 'extends' for generics class.
     List<Integer> test3 = getList(Integer.valueOf(1));
     test3.stream().forEach(System.out::println);
     
     List<Long> test4 = getList(2L);
     test4.stream().forEach(System.out::println);
     
-    // https://qiita.com/rodentia/items/b36d134fa24867ba4d63
+    //////Using multiple 'extends' for generics class.
+    List<Integer> test5 = getList2(Integer.valueOf(1));
+    test5.stream().forEach(System.out::println);
   }
 
   private static <T> T get(T name) {
@@ -54,4 +60,11 @@ public class GenericsOfMethod {
     list.add(id);
     return list;
   }
+
+  private static <T extends Number & Serializable> List<T> getList2(T id) {
+    List<T> list = new ArrayList<>();
+    list.add(id);
+    return list;
+  }
+
 }
