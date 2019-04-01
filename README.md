f649f2ab1b45a895abffc123df970acc12a6442c
fukugit <fukuma1234@gmail.com>
2019-04-01 09:15:21 +0900

    Update Generics explanation.

--- README.md
+++ README.md
@@ -11,14 +11,14 @@ The following sections explain the new feature of java per each java version.
 1. Generics for Method.  
     The below Java codes are defined in [here](src/main/java/jp/co/example/java5/GenericsOfMethod.java).  
 
-    1-1. The following method is standard one for Generics.  
+    1-1. Standard Generics for Generics.  
     ```
      private static <T> T get(T name) {
        return name;
      }
     ```
 
-    1-2. The following method is for List.  
+    1-2. For List.  
     ```
       private static <T> List<T> getList(T name) {
         List<T> list = new ArrayList<>();
@@ -26,8 +26,67 @@ The following sections explain the new feature of java per each java version.
         return list;
       }
     ```
-
-
+    1-3. For Map.  
+    ```
+      Map<String, Integer> map1 = getHashMap();
+      
+      public static <K, V> HashMap<K, V> getHashMap(){
+        return new HashMap<K, V>();
+      }
+    ```
+    1-4. For Map that has extended class.  
+    ```
+      private static <T extends Number> List<T> getList(T id) {
+        List<T> list = new ArrayList<>();
+        list.add(id);
+        return list;
+    }
+    ```
+    1-5. For Wild Card.  
+    ```
+      Set<String> s1 = Set.of("test1", "test2");
+      Set<String> s2 = Set.of("test1");
+      System.out.println("numElementsInCommon result : " + numElementsInCommon(s1, s2));
+      
+      private static int numElementsInCommon(Set<?> s1, Set<?> s2) {
+        int result = 0;
+        for (Object o1 : s1) {
+          if (s2.contains(o1)) {
+            result++;
+          }
+        }
+        return result;
+      }
+    ```
+    1-6. For Wild Card, return object is also Wild Card.  
+    ```
+      Set<String> s1 = Set.of("test1", "test2");
+      Set<String> s2 = Set.of("test1");
+      System.out.println("getElementsInCommon result :" + getElementsInCommon(s1, s2).toString());
+      
+      private static Set<?> getElementsInCommon(Set<?> s1, Set<?> s2) {
+        Set<Object> result = new HashSet<>();
+        for (Object o1 : s1) {
+          if (s2.contains(o1)) {
+            result.add(o1);
+          }
+        }
+        return result;
+      }
+    ```
+    1-7. Wild Card that is something child class.    
+    ```
+      List<Number> list2 = Arrays.asList(1L);
+      System.out.println("The class type : " + getNum(list2).getClass());
+    
+      private static <E> E getNum(Iterable<? extends E> src) {
+        for (E e : src) {
+          return e;
+        }
+        return null;
+      }
+    ```
+    
 ### Java8
 1. [Lambda](src/main/java/jp/co/example/java8/LambdaFeature.java)  
 2. [Function method](src/main/java/jp/co/example/java8/FunctionFeature.java)  
--- src/main/java/jp/co/example/java5/GenericsOfMethod.java
+++ src/main/java/jp/co/example/java5/GenericsOfMethod.java
@@ -1,44 +1,46 @@
 package jp.co.example.java5;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class GenericsOfMethod {
 
   public static void main(String[] args) {
     // The below is good site for Generics.
     // https://qiita.com/rodentia/items/b36d134fa24867ba4d63
-    
+
     ////// Using generics method, whose result is single.
     String test1 = get("Test1");
     System.out.println(test1);
-    
+
     ////// Using generics method, whose result is list.
     List<String> test2 = getList("Test2");
     test2.stream().forEach(System.out::println);
-    
+
     ////// To define the Type inference(型推論) class.
     Map<String, Integer> map1 = getHashMap();
     map1.put("key", 1);
     map1.entrySet().stream().map(e -> e.getKey() + ": " + e.getValue()).forEach(System.out::println);
-    
+
     Map<String, String> map2 = getHashMap();
     map2.put("key", "val");
     map2.entrySet().stream().map(e -> e.getKey() + ": " + e.getValue()).forEach(System.out::println);
-    
+
     ////// Using single 'extends' for generics class.
     List<Integer> test3 = getList(Integer.valueOf(1));
     test3.stream().forEach(System.out::println);
-    
+
     List<Long> test4 = getList(2L);
     test4.stream().forEach(System.out::println);
-    
-    //////Using multiple 'extends' for generics class.
-    List<Integer> test5 = getList2(Integer.valueOf(1));
-    test5.stream().forEach(System.out::println);
+
+    ////// Wild card.
+    Set<String> s1 = Set.of("test1", "test2");
+    Set<String> s2 = Set.of("test1");
+    System.out.println("numElementsInCommon result : " + numElementsInCommon(s1, s2));
+    System.out.println("getElementsInCommon result :" + getElementsInCommon(s1, s2).toString());
+
+    List<Number> list2 = Arrays.asList(1L);
+    System.out.println("The class type : " + getNum(list2).getClass());
   }
 
   private static <T> T get(T name) {
@@ -51,20 +53,40 @@ public class GenericsOfMethod {
     return list;
   }
 
-  public static <K, V> HashMap<K, V> getHashMap(){
+  public static <K, V> HashMap<K, V> getHashMap() {
     return new HashMap<K, V>();
   }
-  
+
   private static <T extends Number> List<T> getList(T id) {
     List<T> list = new ArrayList<>();
     list.add(id);
     return list;
   }
 
-  private static <T extends Number & Serializable> List<T> getList2(T id) {
-    List<T> list = new ArrayList<>();
-    list.add(id);
-    return list;
+  private static int numElementsInCommon(Set<?> s1, Set<?> s2) {
+    int result = 0;
+    for (Object o1 : s1) {
+      if (s2.contains(o1)) {
+        result++;
+      }
+    }
+    return result;
   }
 
+  private static Set<?> getElementsInCommon(Set<?> s1, Set<?> s2) {
+    Set<Object> result = new HashSet<>();
+    for (Object o1 : s1) {
+      if (s2.contains(o1)) {
+        result.add(o1);
+      }
+    }
+    return result;
+  }
+
+  private static <E> E getNum(Iterable<? extends E> src) {
+    for (E e : src) {
+      return e;
+    }
+    return null;
+  }
 }
