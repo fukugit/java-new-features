926479a37fe36a5cc4490af0082b8e714c609be7
fukugit <fukuma1234@gmail.com>
2019-04-11 08:27:19 +0900

    Update Optional feature.

--- src/main/java/jp/co/example/java8/OptionalFeature.java
+++ src/main/java/jp/co/example/java8/OptionalFeature.java
@@ -8,15 +8,22 @@ public class OptionalFeature {
         // https://docs.oracle.com/javase/jp/8/docs/api/java/util/Optional.html
 
         Optional<String> title = Optional.ofNullable(get());
-        String result = title.orElseThrow(() ->new Exception("Value is null. "));
-        System.out.println(result);
+        String result1 = title.orElseThrow(() ->new Exception("Value is null. "));
+        System.out.println(result1);
 
-        if (title.isPresent()) {
-            System.out.println("Not null");
-        }
+        if (title.isPresent()) System.out.println("Value is not null");
+
+        Optional<String> title2 = Optional.ofNullable(null);
+        String result2 = title2.orElse("This is null.");
+        System.out.println(result2);
+
+        Optional<String> title3 = Optional.ofNullable(null);
+        String result3 = title3.orElseGet(() -> get());
+        System.out.println(result3);
+
     }
 
     private static String get() {
-        return "test";
+        return "This is result of get method.";
     }
 }
