4f056e94729cbdc2d57dea770eca2177f2767ba7
fukugit <fukuma1234@gmail.com>
2019-04-26 08:32:50 +0900

    Add some comments.

--- src/main/java/jp/co/example/java8/CollectionsSortFeature.java
+++ src/main/java/jp/co/example/java8/CollectionsSortFeature.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class CollectionsSortFeature {
   public static void main(String[] args) {
 
+    // This is old style. Therefore, we never use it.
     List<String> list0 = Arrays.asList("AAA", "B", "CC");
     Collections.sort(list0, new Comparator<String>() {
       @Override
@@ -14,22 +15,27 @@ public class CollectionsSortFeature {
     });
     list0.stream().forEach(System.out::println);
 
+    // Using Lambda in sort.
     List<String> list1 = Arrays.asList("AAA", "B", "CC");
     list1.sort((s1, s2) -> s1.length() - s2.length());
     list1.stream().forEach(System.out::println);
 
+    // Using "Comparator.comparing", and Lambda.
     List<String> list2 = Arrays.asList("AAA", "B", "CC");
     list2.sort(Comparator.comparing(s -> s.length()));
     list2.stream().forEach(System.out::println);
 
+    // Using "Comparator.comparing", and Method reference as comparing.
     List<String> list3 = Arrays.asList("AAA", "B", "CC");
     list3.sort(Comparator.comparing(String::length));
     list3.stream().forEach(System.out::println);
 
+    // Using "Comparator.comparing", and Method reference as comparingInt.
     List<String> list4 = Arrays.asList("AAA", "B", "CC");
     list4.sort(Comparator.comparingInt(String::length));
     list4.stream().forEach(System.out::println);
 
+    // Comparing for Object list.
     List<Book> books = new ArrayList<>();
     books.add(new Book("AAA", 3000L));
     books.add(new Book("C", 2000L));
