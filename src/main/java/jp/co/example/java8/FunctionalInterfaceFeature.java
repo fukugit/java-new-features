5d5a0808985069c4f67d39587263ae7556c1cd0d
fukugit <fukuma1234@gmail.com>
2019-07-30 08:31:36 +0900

    Add new Functional Interface class.

--- src/main/java/jp/co/example/java8/FunctionalInterfaceFeature.java
+++ src/main/java/jp/co/example/java8/FunctionalInterfaceFeature.java
@@ -1,17 +1,25 @@
 package jp.co.example.java8;
 
 import java.util.Date;
+import java.util.function.Function;
 
 public class FunctionalInterfaceFeature {
 
   public static void main(String[] args) {
+    System.out.println("-------- This is own customized functional interface case, which is Book class. -------");
     Book book1 = (subject) -> {
-      return "Subject : " + subject;
+      return subject.toLowerCase();
     };
     System.out.println(book1.showTitle("AAA"));
 
     Book book2 = get();
     System.out.println(book2.showTitle("bbb"));
+
+    System.out.println("-------- This is Function class case, which Java provides. -------");
+    Function<String, Integer> function1 = s -> Integer.valueOf(s) + 100;
+    System.out.println(function1.apply("100"));
+    Function<String, String> function2 = String::toUpperCase;
+    System.out.println(function2.apply("aaa"));
   }
 
   private static Book get() {
