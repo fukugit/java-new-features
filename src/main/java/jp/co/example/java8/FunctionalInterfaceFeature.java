6ab9fd811221b95d556d46f1834b56664ad6c730
fukugit <fukuma1234@gmail.com>
2019-08-20 08:46:01 +0900

    Add new

--- src/main/java/jp/co/example/java8/FunctionalInterfaceFeature.java
+++ src/main/java/jp/co/example/java8/FunctionalInterfaceFeature.java
@@ -1,6 +1,7 @@
 package jp.co.example.java8;
 
 import java.util.Date;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class FunctionalInterfaceFeature {
@@ -16,10 +17,32 @@ public class FunctionalInterfaceFeature {
     System.out.println(book2.showTitle("bbb"));
 
     System.out.println("-------- This is Function class case, which Java provides. -------");
+
+    // ------------------ Function class ----------------------
+    // 1 line logic.
     Function<String, Integer> function1 = s -> Integer.valueOf(s) + 100;
     System.out.println(function1.apply("100"));
+
+    // Method reference.
     Function<String, String> function2 = String::toUpperCase;
     System.out.println(function2.apply("aaa"));
+
+    // Multi Line logic.
+    Function<String, String> function3 = s -> {
+      String name = s.toUpperCase();
+      return name + ":test";
+    };
+    System.out.println(function3.apply("bbb"));
+
+    // Method argument.
+    System.out.println(getBookTitle(String::toUpperCase));
+    // ------------------ Function class end ----------------------
+
+    // ------------------ Consumer class ----------------------
+    // 1 line logic.
+    Consumer<Integer> consumer1 = s -> System.out.println("Number is " + s);
+    consumer1.accept(100);
+    // ------------------ Consumer class end ----------------------
   }
 
   private static Book get() {
@@ -29,5 +52,9 @@ public class FunctionalInterfaceFeature {
   @FunctionalInterface
   interface Book {
     public String showTitle(String name);
+  }
+
+  private static String getBookTitle(Function<String, String> function) {
+    return function.apply("aaa book");
   }
 }
